df55e12a8ca15ff2c0f0380f23995f13d313369b
Mariia Kryzhalko <kruzhalko93@gmail.com>
2023-05-31 13:49:25 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
-import { store } from "./redux/store";
+import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
